71baacde6f917d94081129d191f39901cced297c
hikaru wakatsuki <hikaru@student.42tokyo.jp>
2026-01-26 15:32:42 +0000

    wak

--- ex0/ft_garden_intro.py
+++ ex0/ft_garden_intro.py
@@ -1,7 +1,15 @@
 def ft_garden_intro():
-	print("=== Welcome to My Garden ===")
-	print("Plant: Rose")
+    name = "Rose"
+    height = 25
+    age = 30
+
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print("")
+    print("=== End of Program ===")
 
 
-if __name__ == '__main__':
-    ft
+if __name__ == "__main__":
+    ft_garden_intro()
