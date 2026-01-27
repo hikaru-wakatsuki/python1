57e124404f9ea58a290078308fcb1aa59530a92b
Hikaru Wakatsuki <hwakatsu@c5r3s12.42tokyo.jp>
2026-01-27 22:48:15 +0900

    wak

---
+++ ex5/ft_plant_types.py
@@ -0,0 +1,6 @@
+def ft_plant_types():
+    print("=== Garden Plant Types ===")
+
+
+if __name__ == "__main__":
+    ft_plant_types()
---
+++ ex6/ft_garden_analytics.py
@@ -0,0 +1,6 @@
+def ft_garden_analytics():
+    print("=== Garden Management System Demo ===")
+
+
+if __name__ == "__main__":
+    ft_garden_analytics()
