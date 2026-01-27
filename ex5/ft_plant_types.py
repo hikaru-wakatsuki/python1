c752ed2f15f991b63c98cabce9c4cb381109171a
Hikaru Wakatsuki <hwakatsu@c5r3s12.42tokyo.jp>
2026-01-27 22:51:40 +0900

    wak

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -1,3 +1,20 @@
+class Plant:
+    """
+    庭にある植物を表すクラス
+
+        name : 植物の名前
+        height : 植物の高さ
+        age : 植物の年齢
+    """
+    def __init__(self, name: str, height: int, age: int):
+        """
+        Plantクラスのインスタンスを初期化するメソッド
+        """
+        self.name = name
+        self.height = height
+        self.age = age
+
+
 def ft_plant_types():
     print("=== Garden Plant Types ===")
 
